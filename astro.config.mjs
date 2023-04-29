ab9a34de60458329f3a904a560f54816687d8867
yousef <yousefsameh24@gmail.com>
2023-04-29 13:46:36 +0300

    deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://yousefelassal.github.io',
+    base: '/my-astro',
+});
